ce24b4bc7b0ad322726b2e3e45c3cf745c614b2c
Kuldar <hi@kuldar.com>
2019-03-07 15:05:27 +0100

    Change image path

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,15 +15,15 @@ export default ({ children }) => (
         <html lang='en' />
         <title>Women World Wide Dev</title>
         <meta name="description" content="DESCRIPTION" />
-        <link rel='canonical' href='SITEURL' />
+        <link rel='canonical' href='https://www.womenworldwide.dev/' />
         <meta name='viewport' content='width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover' />
-        <meta property='og:url' content='SITEURL' />
+        <meta property='og:url' content='https://www.womenworldwide.dev/' />
         <meta property='og:type' content='website' />
         <meta property='og:locale' content='en' />
         <meta property='og:title' content='Women World Wide Dev' />
         <meta property='og:description' content="DESCRIPTION" />
         <meta property='og:site_name' content='Women World Wide Dev' />
-        <meta property='og:image' content='/social.png' />
+        <meta property='og:image' content='https://www.womenworldwide.dev/social.png' />
         <meta property='og:image:width' content='1012' />
         <meta property='og:image:height' content='506' />
         <meta name='twitter:card' content='summary_large_image' />
